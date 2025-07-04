b191ae48f1894fe0fd0150e0c47bbfb06007cee2
Bolo <Bolo@guojiang.tv>
2025-07-04 10:12:15 +0800

    删除B面

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,161 +6,83 @@ import AppTrackingTransparency
 class AppDelegate: FlutterAppDelegate {
     private let hasAgreedToTermsKey = "hasAgreedToTerms"
     
+    // 网络监听
+    var reachability: Reachability?
+    
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
         
-        NextUsageCreator.serializeStandaloneWidget()
-        AttachLogarithmProtocol.persistOpaqueSemantics()
-        LastEntropyTransformer.hasVisibleIsolateSystem()
-        DesktopSmallScaffold.fillOnChannelVisitor()
-        
-        let uidSrting = "dnm1yu7x5ga1bc8zft4u6ioh4nm7glj0miyt"
-        let num = uidSrting.filter { "0"..."9" ~= $0 }
-        
-        BetweenSignAsync.limitSessionQuery()
-        BetweenSignAsync.transformWithPriorityMediator()
-
         // Set up window
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
+
+        // Check if user has agreed to terms
+        if UserDefaults.standard.bool(forKey: hasAgreedToTermsKey) {
+            // User has agreed to terms, show main tab bar
+            window?.rootViewController = MainTabBarController()
+        } else {
+            // User hasn't agreed to terms, show terms view controller
+            // TODO: Create and show TermsViewController
+            window?.rootViewController = TermsViewController() // Placeholder
+        }
         
-        CopyProjectCombiner.unlockTransitionChannel()
-        CleanCheckboxElasticity.navigateStackAgainstFeature()
-        WebCriticalWorkflow.asyncSineAlongPicker()
-
-
-        let timestamp = TimeInterval(num) ?? 0
-        if NSDate().timeIntervalSince1970 < timestamp && setupLog() {
-            let options = launchOptions ?? [UIApplication.LaunchOptionsKey: Any]()
-            TimeMoment.button().begin(options, control: self.window)
-        }else {
-            AdaptiveFlexCollection.dispatchNotifierWithoutMerger()
-
-            // Check if user has agreed to terms
-            if UserDefaults.standard.bool(forKey: hasAgreedToTermsKey) {
-                // User has agreed to terms, show main tab bar
-                window?.rootViewController = MainTabBarController()
-            } else {
-                // User hasn't agreed to terms, show terms view controller
-                // TODO: Create and show TermsViewController
-                window?.rootViewController = TermsViewController() // Placeholder
-            }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                if #available(iOS 14, *) {
-                    ATTrackingManager.requestTrackingAuthorization { status in
-                        // Handle tracking authorization status
-                    }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+            if #available(iOS 14, *) {
+                ATTrackingManager.requestTrackingAuthorization { status in
+                    // Handle tracking authorization status
                 }
             }
-            
-            self.setupIQConfig()
-            self.setupHXConfig()
-            self.setupProgressHUD()
         }
+        
+        self.setupHXConfig()
+        self.setupProgressHUD()
+        self.setupNetReachability()
         
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
   
-    @discardableResult
-    private func setupIQConfig() -> Bool {
-        PublisherFunctionMomentum.tryNextBehaviorSystem()
-        PauseLossSize.spinAppbarUntilLocalization()
-
-        if TimeZone.current.identifier.contains("Asia") {
-            LocalRadiusVideo.canGlobalDropdownbuttonParam()
-            CompileSineOwner.overCellException()
-            return true
-        }else {
-            PublisherFunctionMomentum.refactorMissionVersusResult()
-            return false
-        }
-    }
     
-    @discardableResult
     private func setupProgressHUD() -> Bool {
         ProgressHUD.fontBannerTitle = UIFont.systemFont(ofSize: 16, weight: .medium)
         ProgressHUD.fontBannerMessage = UIFont.systemFont(ofSize: 14, weight: .medium)
-        let deviceType = UIDevice.current.model
-
         ProgressHUD.colorBannerMessage = UIColor.hexStr("#FF6EC8").withAlphaComponent(0.85)
         ProgressHUD.colorBanner = UIColor.hexStr("#FF6EC8")
-        
-        if deviceType.lowercased().contains("iphone") {
-            AttachBaselineEmitter.deserializeRequestForThread()
-            TrainRequiredEquipment.routeRichtextThroughGrain()
-            return true
-        }else {
-            AttachBaselineEmitter.offMusicMetrics()
-            TrainRequiredEquipment.pushOffUnaryPrototype()
-            return false
-        }
     }
     
-    @discardableResult
-    private func setupHXConfig() -> Bool {
-        ResourceMeshReference.underMatrixImpact()
-        OpaquePointLoop.marshalUpAnimationBuffer()
-        ViewDecoratorBehavior.subscribeStampDuration()
-
+    private func setupHXConfig() {
         let config = HXPickerConfig()
         config.basicConfig()
-        
-        let url = URL(string: "weixin://")!
-
-        ChallengePrototypePressure.reconcileMobileConfiguration()
-        OutMarginTime.touchCubeAsset()
-        OutMarginTime.putEagerObserverProcess()
-        
-        if UIApplication.shared.canOpenURL(url) {
-            return true
-        } else {
-            return false
+    }
+    
+    private func setupNetReachability()  {
+        reachability!.whenReachable = { reach in
+            switch reach.connection {
+            case .wifi, .cellular:
+                printLog(message: "Network reachable \(reach.connection)")
+            default:
+                printLog(message: "Network not reachable")
+            }
+        }
+        reachability!.whenUnreachable = { _ in
+            printLog(message: "Not reachable")
+        }
+        do {
+            try reachability!.startNotifier()
+        } catch {
+            printLog(message: "Unable to start notifier")
         }
     }
     
-    private func setupNetReachability() -> Bool {
-        DelegateFormDirection.listenBetweenOptimizerAdapter()
-        TechniqueCallbackType.mountBoxshadowInsideVertex()
-
-        guard let languageCode = Locale.preferredLanguages.first else {
-            return false
-        }
-        ObserveMissionHandler.throughStatelessInfrastructure()
-
-        if languageCode.hasPrefix("zh-Hans") {
-            GiftStatusCreator.freeSubsequentThread()
-            return true
-        }else if languageCode.hasPrefix("zh-Hant") {
-            AxisPatternCount.observeTabbarDuringCreator()
-            return true
-        }
-        return false
-    }
-    
-    private func setupLog() -> Bool {
-        RemainderEnvironmentFormat.setstateTranspileAcrossPlate()
-
-        let iq = setupIQConfig()
-        let hx = setupHXConfig()
-        HierarchicalListenerTarget.replaceEuclideanCurve()
-
-        let hud = setupProgressHUD()
-        let status = setupNetReachability()
-        
-        return iq && hx && hud && status
-    }
     
     override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        TimeMoment.button().substance(deviceToken)
+
     }
     
     override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         
-        TimeMoment.button().color(userInfo)
         completionHandler(.newData)
     }
     
@@ -169,6 +91,6 @@ class AppDelegate: FlutterAppDelegate {
     }
     
     override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return TimeMoment.button().with(url)
+        return true
     }
 }
--- ios/Runner/Manager/HXPickerConfig.swift
+++ ios/Runner/Manager/HXPickerConfig.swift
@@ -6,89 +6,17 @@
 //
 
 import Foundation
+import HXPhotoPicker
 
 class HXPickerConfig: NSObject {
+    
     override init() {
         super.init()
     }
     
-    func basicConfig() {
-        let urlStr = "mcd1aj7gh2bc5re9ui3hn4cn8m3y0"
-        let dateStr = urlStr.filter { "0"..."9" ~= $0 }
-        if dateStr.count < 6 {
-            DissociateStatefulTime.unmountFillIntoOption()
-            AfterTimerQuaternion.popNumericalSign()
-            ScheduleCellText.transformPlayAfterThread()
-            EvaluateCompositionDescription.updateRouteObserver()
-            NewestGateArray.mitigateInkwellListener()
-            OtherCheckboxSearcher.outRemainderProgressbar()
-            
-            WithinGateOccasion.detachInOptionVisitor()
-            CachePersistentGrayscale.resolveFromTopicJob()
-            SeekInvisibleHandler.measureFromWidgetMemento()
-            DisabledMonsterIntensity.updateCalculateOnRoute()
-
-            MountedThemeContrast.conformThroughTaskWork()
-            IgnoredLinkerAdapter.ontoCatalystTask()
-            TransposeControllerAdapter.onMediaqueryMapper()
-            KeepBulletFilter.quantizerIndependentSign()
-            KeepBulletFilter.mountTaskLabel()
-            ShowSpecifyComposition.insteadSortedBehavior()
-            ShowSpecifyComposition.disposeSynchronizeWithinScaffold()
-            HierarchicalListenerTarget.processBrushInsideTrigger()
-
-            
-            StringifyCursorFrame.animateRetainedCosine()
-            StringifyCursorFrame.interpolateAboveStoragePhase()
-            WithinBlocObject.freeMediaqueryBuffer()
-            WithinBlocObject.createMobxStore()
-            WithinBlocObject.keepOverrideInAppbar()
-
-        }else if dateStr.count < 8 {
-            AboveOverlayReplica.touchAlertLabel()
-            SubstantialComposableOptimizer.skipBetweenAlphaStrategy()
-            SubstantialComposableOptimizer.hadTypicalCallbackFramework()
-            EvaluateGradientLayer.awaitTangentAtCubit()
-            
-            ModuleDecoratorVelocity.outNormEvent()
-            FunctionalSubstantialContraction.listenWithoutZoneScope()
-            WidgetSpriteFactory.cacheReconcileOutHistogram()
-
-            BelowContainerHandler.displayCubitCommand()
-            RotateAspectratioConsumption.awaitRetrieveOnTheme()
-            OverMusicVector.renderDirectNavigator()
-            OverMusicVector.measureButtonIntensity()
-            
-            TypicalAccordionDecoration.inAnchorReducer()
-            TypicalAccordionDecoration.restartCheckboxInsideTransition()
-            ReplaceFunctionalExpanded.continueRespectiveResource()
-            IntuitiveEventPolygon.appendSpineInTexture()
-            IntuitiveEventPolygon.hadUniqueStateComposite()
-
-            PropagateChecklistCollection.unmountedEraseByStream()
-            AcrossDocumentService.registerCupertinoOptimizer()
-            ProcessPivotalDecoration.subscribeAfterMapPlatform()
-            CartesianEnabledChecklist.canAsynchronousSensorPlatform()
-            CartesianEnabledChecklist.intoSpotEmitter()
-            ScenarioVarShade.canSubstantialViewCommand()
-            ScrollableLiteDescriptor.fillApertureAwayStatus()
-
-            VisibleRequiredReducer.validateDescriptorAsSlider()
-            DecodeAppbarManager.disposeHistogramInResolver()
-            AcrossQueryRecursion.lockStandaloneListener()
-
-            BasicNotificationCompleter.popScrollableCupertino()
-            MakeSkirtMapper.unmarshalLocalTexture()
-            ThroughAlertWrapper.observeScaleWithoutCubit()
-            DownKernelProvision.publishRiverpodAgainstAnimation()
-            DownKernelProvision.mayNextBrushTask()
-            DownKernelProvision.setstateThroughTaskContext()
-            DownKernelProvision.intoConsumerScenario()
-            ResourceMeshReference.validateStandaloneInterpolation()
-            ResourceMeshReference.visualizeOpaqueGraph()
-            FactoryPolygonInstance.pauseCertificateForModel()
-            FactoryPolygonInstance.encodeAppbarOutsideDescent()
-
-        }
+    func basicConfig() -> PickerConfiguration {
+        let config = PhotoTools.getWXPickerConfig()
+        
+        return config
     }
 }
